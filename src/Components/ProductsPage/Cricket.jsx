e5ac0a9c6429fedcf426f7b85cdd6428966c02eb
aancyboscoj <aancyboscoj@gmail.com>
2024-11-26 12:26:13 +0530

    done from office lap

--- src/Components/ProductsPage/Cricket.jsx
+++ src/Components/ProductsPage/Cricket.jsx
@@ -5,6 +5,8 @@ import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 
 const Cricket = () => {
   const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  console.log("selectedProduct:", selectedProduct);
 
   useEffect(() => {
     const dummyProducts = [
@@ -24,6 +26,8 @@ const Cricket = () => {
     }, 100); // Slight delay before animation starts
   }, []);
 
+  const closeDetails = () => setSelectedProduct(null);
+
   return (
     <div className="cricket-container">
       <div className="grid-container">
@@ -32,6 +36,7 @@ const Cricket = () => {
             key={product.id}
             className="grid-item"
             style={{ animationDelay: `${index * 0.08}s` }} // Staggered animation
+            onClick={() => setSelectedProduct(product)}
           >
             <img src={product.image} alt={product.name} />
             <div className="product-details">
@@ -57,6 +62,45 @@ const Cricket = () => {
           </div>
         ))}
       </div>
+
+      {selectedProduct && (
+        <div className="product-details-page-container slide-in">
+          <div key={selectedProduct.id} className="product-details-content">
+            <img src={selectedProduct.image} alt={selectedProduct.name} />
+            <div className="product-details">
+              <p className="product-name">{selectedProduct.name}</p>
+              <p className="product-desc">{`A cricket bat is a long, rectangular, paddle-shaped piece of equipment used by batters to hit the ball in cricket: 
+Shape: A long, rectangular blade with a short handle 
+Material: Usually made of willow 
+Size: The bat can't be longer than 38 inches (96.5 cm) or wider than 4.25 inches (10.8 cm) 
+Weight: A cricket bat usually weighs between 2.63–3 lb (1.19-1.36 kg) 
+History: The term "cricket bat" was first used in 1622, and the bat's use was first mentioned in 1624 
+Purpose: Batters use the bat to hit the ball, or to avoid a run out gby touching the ground with it `}</p>
+              <p className="product-price">{selectedProduct.price}</p>
+              <div className="rating">
+                {Array.from({ length: 5 }, (_, i) => {
+                  if (i < Math.floor(selectedProduct.rating)) {
+                    return <FaStar key={i} className="star-icon filled" />;
+                  } else if (
+                    i < selectedProduct.rating &&
+                    i === Math.floor(selectedProduct.rating)
+                  ) {
+                    return (
+                      <FaStarHalfAlt key={i} className="star-icon filled" />
+                    );
+                  } else {
+                    return <FaStar key={i} className="star-icon outlined" />;
+                  }
+                })}
+              </div>
+              <div className="buttons-container">
+                <button>Add to Cart</button>
+                <button>Buy Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
